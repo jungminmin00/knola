3a2722869ee770c28d181e17fa4d28754cc8b3ea
jungminmin00 <jjungjj0311@gmail.com>
2023-06-14 10:26:56 +0900

    product모양 만들었

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -3,7 +3,20 @@ import '../scss/product.scss';
 export default function Product(){
     return(
         <div id='product'>
-        Product
+            <div>
+                <figure>
+                    <div>
+                        <img src='' alt='' />
+                    </div>
+                    <figcaption>
+                        <dl>
+                            <dt>이름</dt>
+                            <dd>가격</dd>
+                        </dl>
+                        <p><button type='button'>장바구니 추가</button></p>
+                    </figcaption>
+                </figure>
+            </div>
         </div>
     );
 }
