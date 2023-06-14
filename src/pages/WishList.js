f5010f4e2e0b10671836a658f0895459dfe01ec1
jungminmin00 <jjungjj0311@gmail.com>
2023-06-14 12:26:11 +0900

    wishlist모양 만들기

--- src/pages/WishList.js
+++ src/pages/WishList.js
@@ -1,7 +1,22 @@
+import '../scss/wishList.scss';
+
 export default function WishList(){
     return(
-        <>
-        WishList
-        </>
+        <div id="wishlist">
+            <ul>
+                <li>
+                    <div><img src="" alt="" /></div>
+                    <figure>
+                        <dl>
+                            <dt>이름</dt>
+                            <dd>가격</dd>
+                        </dl>
+                        <p>
+                            <button type="button">delete</button>
+                        </p>
+                    </figure>
+                </li>
+            </ul>
+        </div>
     );
 }
