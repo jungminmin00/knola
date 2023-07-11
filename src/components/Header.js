3fc14e277152b06de260c322fdd5d8979f3ab110
jungminmin00 <jjungjj0311@gmail.com>
2023-07-11 23:41:37 +0900

    반응형으로 만드는 중. (header, content)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import {GiHamburgerMenu} from "react-icons/gi"
+import {AiOutlineCloseSquare} from "react-icons/ai"
 
 export default function Header(){
     return(
@@ -7,31 +9,36 @@ export default function Header(){
                 <h1>
                     <Link to="/"><img src="./images/logo.png" alt="knola" /></Link>
                 </h1>
-                <div className="sub">
-                    <ul>
-                        <li>
-                            <Link to="wishlist">wishlist</Link>
-                        </li>
-                        <li><Link to="login">login</Link></li>
-                    </ul>
-                </div>
-                <nav>
-                    <ul>
-                        <li><Link to="brand">brand</Link></li>
-                        <li className="store">
-                            <Link to="list">store</Link>
-                            <ul className="hide">
-                            {/* <ul > */}
-                                <li><a href="#">all</a></li>
-                                <li><a href="#">granola</a></li>
-                                <li><a href="#">snack</a></li>
-                            </ul>
-                        </li>
-                        <li><Link to="review">review</Link></li>
-                        <li><Link to="event">event</Link></li>
-                    </ul>
-                </nav>
+                
+                <p><GiHamburgerMenu /></p>
             </div>
+            <div>
+                    <div className="sub">
+                        <ul>
+                            <li>
+                                <Link to="wishlist">wishlist</Link>
+                            </li>
+                            <li><Link to="login">login</Link></li>
+                        </ul>
+                    </div>
+                    <nav>
+                        <ul>
+                            <li><Link to="brand">brand</Link></li>
+                            <li className="store">
+                                <Link to="list">store</Link>
+                                <ul className="hide">
+                                {/* <ul > */}
+                                    <li><a href="#">all</a></li>
+                                    <li><a href="#">granola</a></li>
+                                    <li><a href="#">snack</a></li>
+                                </ul>
+                            </li>
+                            <li><Link to="review">review</Link></li>
+                            <li><Link to="event">event</Link></li>
+                        </ul>
+                    </nav>
+                    <p><AiOutlineCloseSquare /></p>
+                </div>
         </header>
     );
 }
